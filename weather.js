707c3336c1de933af38e0a48e722ae2bae224565
ps9610 <ps961001@gmail.com>
2020-08-16 16:34:58 +0900

    weather.js 마무리

--- weather.js
+++ weather.js
@@ -1,13 +1,20 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "23e001ab07326a3a7613d2b897bfba08"
 const COORDS = 'coords';
 
 function getWeather(lat, lng){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
-    ).then(function(response){
-      return response.json();
-    }).then(function(json){
-        console.log(json)
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response){
+        return response.json();
     })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
 }
 
 function saveCoords(coordsObj){
